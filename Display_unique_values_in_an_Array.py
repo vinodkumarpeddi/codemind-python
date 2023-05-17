771c8bff048d49261aca82c5156234bdc8a6a7ac
vinodkumarpeddi <119839782+vinodkumarpeddi@users.noreply.github.com>
2023-05-17 19:09:21 +0530

    n = int(input()) a = list(map(int, input().split())) unique = [] for i in a:     if a.count(i) == 1:         unique.append(i) if len(unique) == 0:     print("-1") else:     for i in unique:         print(i,end=" ")

---
+++ Display_unique_values_in_an_Array.py
@@ -0,0 +1,11 @@
+n = int(input())
+a = list(map(int, input().split()))
+unique = []
+for i in a:
+    if a.count(i) == 1:
+        unique.append(i)
+if len(unique) == 0:
+    print("-1")
+else:
+    for i in unique:
+        print(i,end=" ")
