a72fcc799ea47c01b8a93e278d670a288d5a828d
vinodkumarpeddi <119839782+vinodkumarpeddi@users.noreply.github.com>
2023-05-21 09:17:15 +0530

    def is_fibonacci(num):     a, b = 0, 1     while b < num:         a, b = b, a + b     if b == num:         return True     else:         return False num = int(input()) print(is_fibonacci(num))

---
+++ Fibonacci_or_not.py
@@ -0,0 +1,10 @@
+def is_fibonacci(num):
+    a, b = 0, 1
+    while b < num:
+        a, b = b, a + b
+    if b == num:
+        return True
+    else:
+        return False
+num = int(input())
+print(is_fibonacci(num))
